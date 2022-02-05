d85be066b42ef9ecb674bb16946f37051ec77181
nopalfi <nopalfahriislami@yahoo.com>
2022-02-05 08:55:28 +0800

    rename 'java' package into 'prerequisites'

---
+++ src/prerequisites/CLIPractice.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package prerequisites;
+
+/**
+ * @author nopalfi
+ *
+ */
+class CLIPractice {
+	/*
+	 *  In this CLIPractice.java file, you will learn how to compile a script and run it from a terminal.
+	 *  Make sure you have installed a JDK as the development kit. (I use JDK 17 for by the time i'm making this project).
+	 *  Inside this file there will be a main method that output 'I'm learning Java Programming' in your terminal.
+	 *  Before you start compiling this file, type following command in your terminal:
+	 *  $ java --version
+	 *  If you see a JDK with version in it, then you are good to go.
+	 *  But if you see error that mean it hasn't set properly. You need to figure it out by yourself.
+	 *  After you confirmed, that your JDK has been installed we can start compile this file.
+	 *  First, put this file anywhere in your system, and run:
+	 *  $ javac CLIPractice.java
+	 *  It will produce a file named 'CLIPractice.class' a bytecode version of our script that we can run. If there is no error, then you can start the program by:
+	 *  $ java CLIPractice
+	 *  You can exclude the file extension, it will run just fine, and you will see an output:
+	 *  I'm learning Java Programming.
+	 *  If you see it, congratulation, you just success run a java application. :D 
+	 */
+	public static void main (String[] args) {
+		System.out.println("I'm learning Java Programming.");
+	}
+}
